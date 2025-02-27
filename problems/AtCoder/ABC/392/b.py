5936a5d3c0e7b3d65260e4673e10ea770023cb35
zawa-kun <shult.0219@gmail.com>
2025-02-27 15:12:13 +0900

    ABC392-B

--- problems/AtCoder/ABC/392/b.py
+++ problems/AtCoder/ABC/392/b.py
@@ -1,20 +1,23 @@
-# １からNまで出力していき、もし、Aの要素と一致する下図の時は出力しない.
+N, M = map(int,input().split())# N: Aの最大値, M: Aの要素数.
+A = list(map(int, input().split())) # Aの要素を格納するリスト.
+# N, M = 10, 3
+# A = [3, 9, 2]
 
-# N, M = map(int,input().split())# N: Aの最大値, M: Aの要素数.
-# A = [map(int, input().split())]
+A.sort() # Aを昇順にソート.
 beside_A = [] # Aの要素と一致しないものを格納するリスト.
-N, M = 10, 3
-A = [3, 9, 2]
 
-# Aの要素と一致しないものをbeside_Aに格納.
-for i in range(1, N+1):
-    for j in range(M):
-        if i != A[j]:
-            beside_A.append(i)
-            break
+while len(A) > 0: # Aの要素を全て見るまでループ.
+    for i in range(1, N+1): # 1からNまでの数を見る.
+        if len(A) == 0: 
+            beside_A.append(i) # Aが空の場合は全ての要素をbeside_Aに格納する.
+        elif i != A[0]: 
+            beside_A.append(i) # Aの要素と一致しない場合はbeside_Aに格納する.
+        elif i == A[0]: 
+            A.pop(0)           # Aの要素と一致する場合はpopする.
+        
+        if i > N: break # iがNになったらループを抜ける.
 
-#一致しないモノの最大値を出力.
-print(beside_A[-1])
-#一致しないものを出力.
+# 出力.
+print(len(beside_A))
 for num in beside_A:
     print(num, end=" ")
