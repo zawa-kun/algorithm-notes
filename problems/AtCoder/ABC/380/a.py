b5cf6c10bdbb5a516e70ad24976c17fe3cdbb450
zawa-kun <shult.0219@gmail.com>
2025-03-13 18:23:25 +0900

    〇 ABC380-A

---
+++ problems/AtCoder/ABC/380/a.py
@@ -0,0 +1,15 @@
+N = str(input())
+#１の数が１でないとき、処理終了.
+if N.count("1") != 1:
+    print("No")
+    exit()
+# 2の数が2出ないとき、処理終了.
+if N.count("2") != 2:
+    print("No")
+    exit()
+# 2の数が2出ないとき、処理終了.
+if N.count("3") != 3:
+    print("No")
+    exit()
+
+print("Yes")
