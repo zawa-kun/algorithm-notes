02378d6f21bca150dfe492ce350f83736b72c2fd
zawa-kun <shult.0219@gmail.com>
2025-03-14 13:56:31 +0900

    fix: ABC379-A

--- problems/AtCoder/ABC/379/a.py
+++ problems/AtCoder/ABC/379/a.py
@@ -1,7 +1,7 @@
 N = str(input())
 
 #b,c,aの順番.
-print(N[1], N[2], N[0], end=" ")
-#c, a, b
-print(N[2], N[0], N[1])
+print(N[1],N[2],N[0],sep="", end=" ")
+#c, a, bの順番.
+print(N[2], N[0], N[1], sep="")
 
