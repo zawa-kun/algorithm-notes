d78ab2dd7cab5febcf57abbd89186779ca930aac
zawa-kun <shult.0219@gmail.com>
2025-04-06 01:50:22 +0900

    振り返り ABC373-B

--- problems/AtCoder/ABC/372/b.py
+++ problems/AtCoder/ABC/372/b.py
@@ -25,25 +25,4 @@ for i in range(1,21):
 
             nokori -= 3 ** A[A_idx]
             A_idx += 1
-
-    # if 3 ** i  > M:
-    #     A = [0] * i
-    #     A_idx = 0
-    #     A[A_idx] = i-1
-    #     nokori = M
-    #     # nokori = M - 3 ** i-1 
-    #     while A_idx < i - 1 and nokori > 0:
-            
-    #         nokori -= 3 ** A[A_idx]
-    #         for A_kouho in range(i):
-    #             if nokori - 3 ** A_kouho == 0:
-    #                 A[A_idx+1] = A_kouho
-    #                 # output result
-    #                 print(i)
-    #                 print(*A, sep=' ')
-    #                 exit()
-    #             elif nokori - 3 ** A_kouho > 0 :
-    #                 A[A_idx+1] = A_kouho
-
-    #         A_idx += 1
             
