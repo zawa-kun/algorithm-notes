ca0c6fe9425140257241636036c3de9e04d56615
zawa-kun <shult.0219@gmail.com>
2025-04-12 00:47:22 +0900

    n進数に変換する関数

--- tips/numeral_system.py
+++ tips/numeral_system.py
@@ -16,5 +16,28 @@ def convert_to_binary_number(num: int) -> str:
 
     return binary_number
 
+# 10進数 -> n進数
+def convert_to_n_number(num: int, numeral: int) -> str:
+    if num == 0:
+        return '0'
+    
+    binary_number = ''
+    while num >= numeral:
+        binary_number += str(num % numeral)
+        if num % numeral == 0:
+            num //= numeral
+        else:
+            num = (num - num % numeral) // numeral
+    
+    binary_number += str(num)
+    binary_number = binary_number[::-1]
+
+    return binary_number
+
+
 if __name__ == '__main__':
+    print('-----convert_to_binary_number()-----')
     print(convert_to_binary_number(10))
+    print('-----convert_to_n_number()-----')
+    print(convert_to_n_number(10, 4))
+    
