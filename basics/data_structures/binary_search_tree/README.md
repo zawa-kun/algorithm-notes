e4e5dc5adecfd4b37c97a1eeacd249945e83e913
zawa-kun <shult.0219@gmail.com>
2025-04-22 15:32:33 +0900

    binary search tree(inorder, search)

---
+++ basics/data_structures/binary_search_tree/README.md
@@ -0,0 +1,5 @@
+# 二分探索木( Binary Search Tree )
+### いろんな木の見方
+**Inorder**: Left, Root, Right
+**Preorder**: Roor, Left, Right
+**Postorder**: Left, Right, Root
--- basics/data_structures/binary_search_tree/binary_search_tree.py
+++ basics/data_structures/binary_search_tree/binary_search_tree.py
@@ -4,7 +4,7 @@ class Node(object):
         self.left = None
         self.right = None
 
-
+# 要素の挿入
 def insert(node: Node, value: int) -> Node:
     if node is None:
         return Node(value)
@@ -16,6 +16,30 @@ def insert(node: Node, value: int) -> Node:
         node.right = insert(node.right, value)
     return node
 
+# 昇順で表示
+def inorder(node: Node) -> None:
+    # いろんな木の見方
+    # Inorder Left, Root, Right
+    # Preorder Roor, Left, Right
+    # Postorder Left, Right, Root
+    if node is not None:
+        inorder(node.left)
+        print(node.value)
+        inorder(node.right)
+
+
+def search(node: Node, value: int) -> bool:
+    if node is None:
+        return False
+    
+    if node.value == value:
+        return True
+    
+    elif value < node.value:
+        return search(node.left, value)
+    else:
+        return search(node.right, value)
+
 
 
 if __name__ == '__main__':
@@ -23,7 +47,14 @@ if __name__ == '__main__':
     root = insert(root, 3)
     root = insert(root, 6)
     root = insert(root, 5)
-    print(root.value)               # 3
-    print(root.right.value)         # 6
-    print(root.right.left.value)    # 5
+    root = insert(root, 7)
+    root = insert(root, 1)
+    root = insert(root, 10)
+    root = insert(root, 2)
+    # inorder(root)
+    print(search(root, 10))
+    print(search(root, 11))
+    # print(root.value)               # 3
+    # print(root.right.value)         # 6
+    # print(root.right.left.value)    # 5
     
