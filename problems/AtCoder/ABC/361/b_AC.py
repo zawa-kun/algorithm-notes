8f56f673beda8165768fc4729af2d599291803f9
zawa-kun <shult.0219@gmail.com>
2025-05-11 14:19:12 +0900

    fix

--- problems/AtCoder/ABC/361/b_AC.py
+++ problems/AtCoder/ABC/361/b_AC.py
@@ -1,6 +1,7 @@
 x1, y1, z1, x2, y2, z2 = map(int, input().split())
 x3, y3, z3, x4, y4, z4 = map(int, input().split())
 
+# 重なりがあるかを判定
 def f(l1, r1, l2, r2):
     # [l1, r1] と[l2, r2]`の共通部分の長さが正ならTrue
     return not (r1<=l2 or r2<=l1)
